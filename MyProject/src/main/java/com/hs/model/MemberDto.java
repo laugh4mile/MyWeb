702cfdecd6eaff59cbf2c2639581b9c766bc0474
laugh4mile <laugh4mile@naver.com>
2021-03-03 23:29:18 +0900

    MemberDto

--- MyProject/src/main/java/com/hs/model/MemberDto.java
+++ MyProject/src/main/java/com/hs/model/MemberDto.java
@@ -11,6 +11,30 @@ public class MemberDto {
 		this.password = password;
 		this.name = name;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	
 }
