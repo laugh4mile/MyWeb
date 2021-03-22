e8e034d8aab38621574ce82d7d007470e94e3ccb
laugh4mile <laugh4mile@naver.com>
2021-03-22 23:44:10 +0900

    feat: MemberMapper

--- MyProject/src/main/java/com/hs/service/MemberServiceImpl.java
+++ MyProject/src/main/java/com/hs/service/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package com.hs.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.hs.model.MemberDto;
+import com.hs.model.mapper.MemberMapper;
 
 public class MemberServiceImpl implements MemberService {
-
+	
+	@Autowired
+	MemberMapper memberMapper;
+	
 	@Override
 	public void join(MemberDto dto) {
 		// TODO Auto-generated method stub 
